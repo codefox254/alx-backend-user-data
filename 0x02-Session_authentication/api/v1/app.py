0b168540e8b80da8fecc2b21665fd022fc3caf44
codefox254 <91884653+codefox254@users.noreply.github.com>
2024-10-19 02:46:47 +0300

    Update app.py

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -24,20 +24,24 @@ elif auth_type == "auth":
     from api.v1.auth.auth import Auth  # Import Auth
     auth = Auth()  # Create an instance of Auth
 
+
 @app.errorhandler(404)
 def not_found(error) -> str:
     """ Not found handler """
     return jsonify({"error": "Not found"}), 404
+
 
 @app.errorhandler(401)
 def unauthorized(error) -> str:
     """ Unauthorized handler """
     return jsonify({"error": "Unauthorized"}), 401
 
+
 @app.errorhandler(403)
 def forbidden(error) -> str:
     """ Forbidden handler """
     return jsonify({"error": "Forbidden"}), 403
+
 
 # Before request handler to validate requests
 @app.before_request
@@ -47,7 +51,9 @@ def before_request_handler():
         return
 
     # Define paths that do not require authentication
-    excluded_paths = ['/api/v1/status/', '/api/v1/unauthorized/', '/api/v1/forbidden/']
+    excluded_paths = [
+        '/api/v1/status/', '/api/v1/unauthorized/', '/api/v1/forbidden/'
+    ]
 
     # Check if the request requires authentication
     if not auth.require_auth(request.path, excluded_paths):
@@ -57,12 +63,10 @@ def before_request_handler():
     if auth.authorization_header(request) is None:
         abort(401)
 
-    # Assign the authenticated user to request.current_user
-    request.current_user = auth.current_user(request)
-
     # Check if the current user is present
-    if request.current_user is None:
+    if auth.current_user(request) is None:
         abort(403)
+
 
 if __name__ == "__main__":
     host = getenv("API_HOST", "0.0.0.0")
