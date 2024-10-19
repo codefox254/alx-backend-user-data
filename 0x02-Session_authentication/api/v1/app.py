19c8cf5a733f63a9df1467e6c0b6ab7cf8778bae
codefox254 <91884653+codefox254@users.noreply.github.com>
2024-10-19 04:18:30 +0300

    Update app.py

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -4,10 +4,11 @@ Route module for the API
 """
 
 from os import getenv
-from api.v1.views import app_views
 from flask import Flask, jsonify, abort, request
 from flask_cors import CORS
+from api.v1.views import app_views
 
+# Create a Flask application
 app = Flask(__name__)
 app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
@@ -43,7 +44,6 @@ def forbidden(error) -> str:
     """Forbidden handler."""
     return jsonify({"error": "Forbidden"}), 403
 
-# Before request handler to validate requests
 @app.before_request
 def before_request_handler():
     """Handler for filtering requests before processing them."""
