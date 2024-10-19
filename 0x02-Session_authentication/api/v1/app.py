47292d9dbdeb1dc0e73fa3ee1923d56c955ffc3d
root <frankode254@gmail.com>
2024-10-19 04:36:32 +0300

    adding the remaining tasks part2

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -8,7 +8,6 @@ from api.v1.views import app_views
 from flask import Flask, jsonify, abort, request
 from flask_cors import CORS
 
-
 app = Flask(__name__)
 app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
@@ -25,28 +24,27 @@ if auth_type == "basic_auth":
 elif auth_type == "session_auth":
     from api.v1.auth.session_auth import SessionAuth  # Import SessionAuth
     auth = SessionAuth()  # Create an instance of SessionAuth
+elif auth_type == "session_exp_auth":  # Add condition for SessionExpAuth
+    from api.v1.auth.session_exp_auth import SessionExpAuth  # Import SessionExpAuth
+    auth = SessionExpAuth()  # Create an instance of SessionExpAuth
 elif auth_type == "auth":
     from api.v1.auth.auth import Auth  # Import Auth
     auth = Auth()  # Create an instance of Auth
-
 
 @app.errorhandler(404)
 def not_found(error) -> str:
     """Not found handler."""
     return jsonify({"error": "Not found"}), 404
 
-
 @app.errorhandler(401)
 def unauthorized(error) -> str:
     """Unauthorized handler."""
     return jsonify({"error": "Unauthorized"}), 401
 
-
 @app.errorhandler(403)
 def forbidden(error) -> str:
     """Forbidden handler."""
     return jsonify({"error": "Forbidden"}), 403
-
 
 # Before request handler to validate requests
 @app.before_request
@@ -78,7 +76,6 @@ def before_request_handler():
     # Check if the current user is present
     if request.current_user is None:
         abort(403)
-
 
 if __name__ == "__main__":
     host = getenv("API_HOST", "0.0.0.0")
